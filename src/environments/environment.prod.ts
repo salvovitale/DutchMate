8b469667add660e534d6834952b2102c32fff14c
Salvatore Vitale <salvatore.vitale@me.com>
2020-06-01 23:33:47 +0200

    re-add  key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   signInUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword',
   sendResetPasswordUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode',
   refreshTokenUrl:'https://securetoken.googleapis.com/v1/token',
-  firebaseAPIKey: '',
+  firebaseAPIKey: 'AIzaSyBOWwxwAdPRg0ktcDhdLI8SwT8CngOp7E4',
   authDataStoreKey: 'dutchMateAuthData'
 };
